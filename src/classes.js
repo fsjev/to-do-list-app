848cdec54fa6f997423935c12d104d5e2c78f85e
Jevaughn Vickers <fullstackjev@gmail.com>
2023-08-31 11:28:42 -0400

    move classes

---
+++ src/classes.js
@@ -0,0 +1,46 @@
+class Todo{
+    constructor(title, dueDate){
+        this.title = title;
+        this.dueDate = format(new Date(dueDate), "MMM dd, yyyy");
+    };
+};
+
+
+class TodoContainer{
+    constructor(){
+        this.todos = [];
+    };
+
+    inTodos(todo){
+        return this.todos.some(item => item === todo);
+    };
+
+    addTodo(newTodo){
+        this.todos.push(newTodo);
+    };
+
+    deleteTodo(todo){
+        this.todos = this.todos.filter(item => item !== todo);
+    };
+};
+
+
+class CategoryContainer{
+    constructor(){
+        this.categories = [];
+    };
+
+    inCategories(category){
+        return this.categories.some(item => item === category);
+    };
+
+    addCategory(newCategory){
+        this.categories.push(newCategory);
+    };
+
+    deleteCategory(category){
+        this.todos = this.categories.filter(item => item !== category);
+    };
+};
+
+export { Todo, TodoContainer, CategoryContainer};
--- src/script.js
+++ src/script.js
@@ -1,54 +1,7 @@
 import { format } from "date-fns"; 
+import { Todo, TodoContainer, CategoryContainer } from "./classes";
 
 
-class Todo{
-    constructor(title, dueDate){
-        this.title = title;
-        this.dueDate = format(new Date(dueDate), "MMM dd, yyyy");
-    };
-};
-
-
-class TodoContainer{
-    constructor(){
-        this.todos = [];
-    };
-
-    inTodos(todo){
-        return this.todos.some(item => item === todo);
-    };
-
-    addTodo(newTodo){
-        this.todos.push(newTodo);
-    };
-
-    // getTodo(){
-
-    // };
-
-    deleteTodo(todo){
-        this.todos = this.todos.filter(item => item !== todo);
-    };
-};
-
-
-class CategoryContainer{
-    constructor(){
-        this.categories = [];
-    };
-
-    inCategories(category){
-        return this.categories.some(item => item === category);
-    };
-
-    addCategory(newCategory){
-        this.categories.push(newCategory);
-    };
-
-    deleteCategory(category){
-        this.todos = this.categories.filter(item => item !== category);
-    };
-};
 
 const mainTodoContainer = new TodoContainer();
 
