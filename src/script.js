fce036ec6803bc7895f1fbf02b2e824ba4ca619d
Jevaughn Vickers <fullstackjev@gmail.com>
2023-08-31 14:14:37 -0400

    move global variables into module

--- src/script.js
+++ src/script.js
@@ -1,17 +1,13 @@
 import { format } from "date-fns"; 
 import { Todo, TodoContainer, CategoryContainer } from "./classes.js";
 
-const CATEGORYCONTAINER = new CategoryContainer();
 
-const MAINTODOCONTAINER = new TodoContainer();
-
-CATEGORYCONTAINER.addCategory(MAINTODOCONTAINER);
 
 const UIController = (() => {
     // allow user to interact with the program via UI
     // read the state of the program and display it in the browser
-    // const createCatBtn = document.getElementById("create-category");
-
+    const createCatBtn = document.getElementById("create-category");
+    
 })();
 
 
@@ -19,16 +15,19 @@ const UIController = (() => {
 const AppFunctions = (() => {
     // 08312023
     const today = format(new Date(), "MMddyyyy");
+    const CATEGORYCONTAINER = new CategoryContainer();
+    const MAINTODOCONTAINER = new TodoContainer();
+    CATEGORYCONTAINER.addCategory(MAINTODOCONTAINER);
+
+    const createTodo = (title, dueDate) => {
+
+        return new Todo(title, dueDate);
+    };
+
+    const createTodoContainer = () => {
+
+        return new TodoContainer();
+    };
     
 })();
-
-function createTodo(title, dueDate){
-
-    return new Todo(title, dueDate);
-};
-
-function createTodoContainer(){
-
-    return new TodoContainer();
-};
 
