00185848713d9002131a0f0756c565d685de2c4c
Jevaughn Vickers <fullstackjev@gmail.com>
2023-08-31 12:55:17 -0400

    start building UIController module

--- src/script.js
+++ src/script.js
@@ -7,6 +7,13 @@ const MAINTODOCONTAINER = new TodoContainer();
 
 CATEGORYCONTAINER.addCategory(MAINTODOCONTAINER);
 
+const UIController = (() => {
+    // allow user to interact with the program via UI
+    // read the state of the program and display it in the browser
+    const createCatBtn = document.getElementById("create-category");
+    
+})();
+
 function createTodo(){
 
     // title, dueDate
